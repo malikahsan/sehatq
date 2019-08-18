4018c5ca64a25574d5f59c655a05e76c04b29ec0
Ahsan Malik <sann.lix@gmail.com>
2019-08-18 12:45:19 +0700

    third commit

--- app/models/schedule_booked.rb
+++ app/models/schedule_booked.rb
@@ -13,6 +13,6 @@ class ScheduleBooked < ApplicationRecord
 
   def count_booked_per_day
     count_doctor_shcedule_per_day = self.class.where(doctor_id: self.doctor_id, created_at: [Date.today.beginning_of_day..Date.today.end_of_day]).count
-    self.try(:errors).try(:add, :base, "Opps, this doctor have been maximum booked for today. Please booked next day.") if count_doctor_shcedule_per_day >= 6
+    self.try(:errors).try(:add, :base, "Opps, this doctor have been maximum booked for today. Please booked next day.") if count_doctor_shcedule_per_day >= 10
   end
 end
